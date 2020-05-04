3f1881c1a35be380fd426297d1427c9de246833e
Sergey Meshchankin <smeshchankin@gmail.com>
2020-05-04 23:11:14 +0300

    [PJS-001] Implement generateId method

--- src/populator.js
+++ src/populator.js
@@ -1,6 +1,8 @@
 window.populator = (function() {
     let populator = {
-        
+        generateId: function(prefix) {
+            return prefix + Math.round(Math.random()*1e8).toString(16);
+        }
     };
 
     return populator;
