6fd307eb61b85881e0748137eaf86d764b1dc8da
Sergey Meshchankin <smeshchankin@gmail.com>
2020-05-04 23:24:55 +0300

    [PJS-002] Implement populate method

--- src/populator.js
+++ src/populator.js
@@ -2,8 +2,38 @@ window.populator = (function() {
     let populator = {
         generateId: function(prefix) {
             return prefix + Math.round(Math.random()*1e8).toString(16);
+        },
+        populate: function(templateSelector, data) {
+            let template = document.querySelector(templateSelector);
+            if (!data || template) {
+                return;
+            }
+
+            if (data.length) {
+                populateFromArray(template, data);
+            } else {
+                populateFromObject(template, data)
+            }
         }
     };
+
+    function populateFromObject(elem, obj) {
+        Object.keys(data).forEach(function(key) {
+            let value = data[key];
+            elem.innerHTML = elem.innerHTML.replace('{{' + key + '}}', value);
+        });
+    }
+
+    function populateFromArray(template, arr) {
+        let parentElem = template.parentElement;
+        parentElem.removeChild();
+
+        for (let i = 0; i < arr.length; i++) {
+            let elem = template.cloneNode(true);
+            populateFromObject(elem, arr[i]);
+            parentElem.appendChild(elem);
+        }
+    }
 
     return populator;
 }());
